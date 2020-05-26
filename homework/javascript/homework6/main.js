5f6cb933bfd4886c34d7b8dd6f2cf06922e753b5
ylevina <j.levina88@gmail.com>
2020-05-26 22:44:34 +0300

    js homework6

--- homework/javascript/homework6/main.js
+++ homework/javascript/homework6/main.js
@@ -1,12 +1,5 @@
 console.log(filterBy(['hello', 'world', 23, '23', null], 'string'));
 
-function filterBy(inputArray, type) {
-    let outputArray = [];
-    inputArray.forEach(element => {
-        if ((typeof element) !== type){
-            outputArray.push(element);
-        }            
-    });
-
-    return outputArray;
+function filterBy(inputArray, type) {   
+    return inputArray.filter(element => typeof element!== type)
 }
